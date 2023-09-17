c9c894906d5514ec19bdd979fabb316aaa719c85
Vit Perov <vitaliy.perov88@gmail.com>
2023-09-17 18:27:27 +0300

    "homework is ready"

---
+++ src/test/java/SearchExampleJU.java
@@ -0,0 +1,58 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchExampleJU {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1500x980";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("https://demoqa.com/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#firstName").setValue("Vit");
+        $("#lastName").setValue("Perov");
+        $("#userEmail").setValue("vit.perov@gmail.com");
+        $(byText("Male")).click();
+        $("#userNumber").setValue("9800000000");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-dropdown-container").$(byText("October")).click();
+        $(".react-datepicker__year-select").selectOption("1988");
+        $(".react-datepicker__day--002").click();
+        $("#subjectsInput").setValue("Economics").pressEnter();
+        $(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("photo.jpg");
+        $("#currentAddress").setValue("Russia");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $(".modal-dialog").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-content").shouldHave(
+                text("Vit Perov"),
+                text("vit.perov@gmail.com"),
+                text("Male"),
+                text("9800000000"),
+                text("2 October,1988"),
+                text("Economics"),
+        //        text("photo.jpg"),
+                text("Russia"),
+                text("NCR Delhi")
+        );
+    }
+}
